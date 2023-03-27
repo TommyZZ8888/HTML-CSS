a66bd43a906cd9575e40fca146ee01fdb38fd2dc
TommyZZ8888 <1950442495@qq.com>
2023-03-27 15:06:51 +0800

    html-css练手

---
+++ greedySnake/game/gameStart.js
@@ -0,0 +1,8 @@
+var body = document.getElementById("greedySnake\static\body.png");
+var down = document.getElementById("greedySnake\static\down.png");
+var up = document.getElementById("greedySnake\static\up.png");
+var left = document.getElementById("greedySnake\static\left.png");
+var right = document.getElementById("greedySnake\static\right.png");
+var food = document.getElementById("D:\test\js\greedySnake\static\food.png");
+var header = document.getElementById("greedySnake\static\header.png");
+
